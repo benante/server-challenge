26a24af09af67530230a484f06de9bff9e72400a
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-09-16 12:57:13 +0100

    challenge 1 & 2

--- src/server.js
+++ src/server.js
@@ -3,3 +3,27 @@ const express = require("express");
 const server = express();
 
 module.exports = server;
+
+server.get("/", (req, res) => {
+  res.send("<h1>Hello Express</h1>");
+});
+
+server.get("/colour", (req, res) => {
+  const hex = req.query.hex || "ffffff";
+  res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+       <style>
+       body {
+           background-color: #${hex};
+        }
+        </style>
+    </head>
+    <body>
+        
+    </body>
+    </html>`);
+});
