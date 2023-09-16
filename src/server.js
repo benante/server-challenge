f2ede6c6180483e1a1cf8477e96cbef0158348fa
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-09-16 13:52:36 +0100

    challenge 3.not really clear to understand

--- src/server.js
+++ src/server.js
@@ -9,21 +9,17 @@ server.get("/", (req, res) => {
 });
 
 server.get("/colour", (req, res) => {
-  const hex = req.query.hex || "ffffff";
-  res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-       <style>
-       body {
-           background-color: #${hex};
-        }
-        </style>
-    </head>
-    <body>
-        
-    </body>
-    </html>`);
+  const hex = req.query.hex || "ffffff"; // defaults to white
+  const html = `
+    <style>
+      body {
+        background-color: #${hex};
+      }
+    </style>
+    <form>
+      <label for="hex">Enter hex</label>
+      <input name="hex" value="${hex}">
+    </form>
+  `;
+  res.send(html);
 });
